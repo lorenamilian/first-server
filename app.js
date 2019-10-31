317220e44996d57bab402db2f8a0e803f8af3a96
lorena <lorenalies2312@gmail.com>
2019-10-31 13:26:14 -0400

    learnig how to write html in our own server

--- app.js
+++ app.js
@@ -5,11 +5,28 @@ const app = express();
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+// middleware
+app.use(express.static('./public'));
+
 app.get('/', function(request, response){
 
     response.render('about.ejs');
 
 });
+
+app.get('/contact', function(request, response){
+
+    response.render('contact.ejs');
+
+});
+
+app.get('/project', function(request, response){
+
+    response.render('project.ejs');
+
+});
+
+
 
 //
 app.listen(3000, function () {
